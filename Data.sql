c66ac3be2cca9f3b849ddc361a514f55ba2a19cb
Sagar C A <44050943+sag-007@users.noreply.github.com>
2020-06-07 22:11:22 +0530

    Add files via upload

---
+++ Data.sql
@@ -0,0 +1,16 @@
+
+insert into user values (1,'sagar', 'sagarca77@gmail.com')
+insert into user values (2,'Dhoni', 'captian@india.com')
+insert into user values (3,'Modi', 'PM@india.com')
+
+
+insert into category values (1,'Travel')
+insert into category values (2,'Auto Loan')
+insert into category  values (3,'Travel')
+
+
+insert into expense values (100,'Mumbai Business Trip','2019-06-16T17:00:00.000Z','Mumbai',1,1)
+insert into expense values (101,'Yamaha R15 Payment','2019-06-15T15:00:00.000Z','Bangalore',2,2)
+insert into expense values(102,'Hampi Trip With Family','2019-06-15T15:00:00.000Z','Hampi',3,1)
+ 
+
